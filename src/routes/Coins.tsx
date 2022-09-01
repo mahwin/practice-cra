c34bfd3c560785251cc712e5cfd291092eef10d6
mahwin <mahwin7085@gmail.com>
2022-09-01 17:29:11 +0900

    Add  get api call from coin-paprika

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,8 +1,115 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+const Container = styled.div`
+  padding: 0px 10px;
+  max-width: 560px;
+  margin: 0 auto;
+`;
+
+const Header = styled.div`
+  height: 15vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+  font-size: 50px;
+`;
+
+const CoinList = styled.ul``;
+const Coin = styled.li`
+  background-color: white;
+  color: ${(props) => props.theme.bgColor};
+  padding: 20px;
+  margin-top: 10px;
+  border-radius: 15px;
+  cursor: pointer;
+
+  a {
+    transition: color 0.2s ease-in;
+  }
+  &:hover {
+    a {
+      color: ${(props) => props.theme.accentColor};
+    }
+  }
+`;
+
+const coins = [
+  {
+    id: "btc-bitcoin",
+    name: "Bitcoin",
+    symbol: "BTC",
+    rank: 1,
+    is_new: false,
+    is_active: true,
+    type: "coin",
+  },
+  {
+    id: "eth-ethereum",
+    name: "Ethereum",
+    symbol: "ETH",
+    rank: 2,
+    is_new: false,
+    is_active: true,
+    type: "coin",
+  },
+  {
+    id: "hex-hex",
+    name: "HEX",
+    symbol: "HEX",
+    rank: 3,
+    is_new: false,
+    is_active: true,
+    type: "token",
+  },
+];
+
+interface CoinInterface {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+}
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
+
 function Coins() {
+  const [coins, setCoins] = useState<CoinInterface[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("https://api.coinpaprika.com/v1/coins");
+      const json = await response.json();
+      setCoins(json.slice(0, 100));
+      setIsLoading(false);
+    })();
+  }, []);
   return (
-    <>
-      <h1>Coins</h1>
-    </>
+    <Container>
+      <Header>
+        <Title>코인</Title>
+      </Header>
+      <CoinList>
+        {isLoading ? (
+          <Loader>Loading ...</Loader>
+        ) : (
+          coins.map((coin) => (
+            <Coin key={coin.id}>
+              <Link to={"none"}>{coin.name} &rarr;</Link>
+            </Coin>
+          ))
+        )}
+      </CoinList>
+    </Container>
   );
 }
 
