1c99bc64d0618df17b9f6738d5ac50d6249937e2
mahwin <mahwin7085@gmail.com>
2022-09-05 23:17:16 +0900

    Refactoring useState => selector(recoil)

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -41,3 +41,10 @@ function ToDo({ text, category, id }: IToDo) {
 }
 
 export default ToDo;
+
+let [num1, num2] = [8, 33];
+const x1 = Math.floor(num1 / 4);
+const y1 = num1 % 4 === 0 ? 4 : num1 % 4;
+const x2 = Math.floor(num2 / 4);
+const y2 = num2 % 4 === 0 ? 4 : num2 % 4;
+console.log(Math.abs(x1 - x2) + Math.abs(y1 - y2));
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,17 +1,34 @@
 import { useRecoilValue } from "recoil";
-import { toDoState } from "./atom";
+import { toDoSelector, toDoState } from "./atom";
 import CreateToDo from "./CreateToDO";
 import ToDo from "./ToDo";
 
 function ToDoList() {
-  const toDos = useRecoilValue(toDoState);
+  const [toDo, doing, done] = useRecoilValue(toDoSelector);
+
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
       <CreateToDo />
+      <h2>To Do</h2>
+      <hr />
       <ul>
-        {toDos.map((toDo) => (
+        {toDo.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </ul>
+      <hr />
+      <h2>Toing</h2>
+      <ul>
+        {doing.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </ul>
+      <hr />
+      <h2>Done</h2>
+      <ul>
+        {done.map((toDo) => (
           <ToDo key={toDo.id} {...toDo} />
         ))}
       </ul>
