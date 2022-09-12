2088c2d0d5b9d9412763dd25fbd23d53680ebc37
mahwin <mahwin7085@gmail.com>
2022-09-12 19:59:56 +0900

    Reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,5 @@
-import React from "react";
-import { useRecoilState, useRecoilValue } from "recoil";
-import { hourSelector, minuteState } from "./atoms";
-
 function App() {
-  const [minutes, setMinutes] = useRecoilState(minuteState);
-  const [hours, setHours] = useRecoilState(hourSelector);
-  const onMinuteChange = (event: React.FormEvent<HTMLInputElement>) => {
-    setMinutes(+event.currentTarget.value);
-  };
-
-  const onHourChange = (event: React.FormEvent<HTMLInputElement>) => {
-    setHours(+event.currentTarget.value);
-  };
-  return (
-    <div>
-      <input
-        type="number"
-        placeholder="Minutes"
-        value={minutes}
-        onChange={onMinuteChange}
-      />
-      <input
-        onChange={onHourChange}
-        type="number"
-        placeholder="Hours"
-        value={hours}
-      />
-    </div>
-  );
+  return <></>;
 }
 
 export default App;
