7381df35500c726fd4536e246df9357e518a9421
mahwin <mahwin7085@gmail.com>
2022-09-13 17:47:36 +0900

    Refactory dnd component

---
+++ src/Components/DragabbleCard.tsx
@@ -0,0 +1,32 @@
+import { Draggable } from "react-beautiful-dnd";
+import styled from "styled-components";
+
+const Card = styled.div`
+  border-radius: 5px;
+  margin-bottom: 5px;
+  padding: 10px 10px;
+  background-color: ${(props) => props.theme.cardColor};
+`;
+
+interface IDragabbleCradProrps {
+  index: number;
+  toDo: string;
+}
+
+function DraggableCard({ toDo, index }: IDragabbleCradProrps) {
+  return (
+    <Draggable key={toDo} draggableId={toDo} index={index}>
+      {(magic) => (
+        <Card
+          ref={magic.innerRef}
+          {...magic.dragHandleProps}
+          {...magic.draggableProps}
+        >
+          {toDo}
+        </Card>
+      )}
+    </Draggable>
+  );
+}
+
+export default DraggableCard;
