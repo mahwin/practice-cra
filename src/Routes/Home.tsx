f1f76fd52e1d48a67691b8ca01fb73507e40ac08
youseock <mahwin7085@gmail.com>
2022-09-21 16:15:17 +0900

    Feat : Header component

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -2,7 +2,7 @@ import Header from "../Components/Header";
 
 function Home() {
   return (
-    <div style={{ backgroundColor: "black", height: "200vh" }}>
+    <div style={{ backgroundColor: "white", height: "200vh" }}>
       <Header />
     </div>
   );
